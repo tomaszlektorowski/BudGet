aabaec087e3eb4651ece3dfddd2c6179dfd4a58f
Tomek <lektor1993@wp.pl>
2018-12-04 17:21:29 +0100

    push-i-did-again

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function () {
         this.value = value;
     };
 
+    var calculateTotal = function (type) {
+        var sum = 0;
+        data.allItems[type].forEach(function (cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    }
+
     var data = {
         allItems: {
             exp: [],
@@ -21,7 +29,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     }
 
     return {
@@ -45,8 +55,24 @@ var budgetController = (function () {
             data.allItems[type].push(newItem);
             //return the new element
             return newItem;
+        },
+        calculateBudget: function () {
+            calculateTotal('exp');
+            calculateTotal('inc');
+            // calculate total income and expenses
 
-
+            //calculate the budget:income-expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            // calculate the percentage of income that we spend
+            data.percentage = Math.round((data.totals.exp / data.totals.inc)*100);
+        },
+        getBudget:function(){
+            return {
+                budget:data.budget,
+                totalInc:data.totals.inc,
+                totalExp:data.totals.exp,
+                percentage:data.percentage
+            }
         }
     };
 
@@ -76,7 +102,7 @@ var UIController = (function () {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, //inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
 
         },
@@ -117,17 +143,17 @@ var UIController = (function () {
             newHtml = newHtml.replace('%value%', obj.value);
 
             //insert html into dom
-            document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
         },
 
-        clearFields:function(){
-            var fields,fieldsArr;
-            fields=document.querySelectorAll(DOMstrings.inputDescription+', '+DOMstrings.inputValue);
+        clearFields: function () {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
 
-            fieldsArr=Array.prototype.slice.call(fields);
-            fieldsArr.forEach(function(current,index,array){
-                    current.value="";
+            fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = "";
 
             });
 
@@ -155,29 +181,44 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
 
 
+    var updateBudget = function () {
 
+        // 1.Calculate the budget
+        budgetCtrl.calculateBudget();
+        // 2.return budget
+        var budget=budgetCtrl.getBudget();
+        console.log(budget);
+        // 3.Display budget to the UI
+
+    };
 
     var ctrlAddItem = function () {
         var input, newItem;
         // 1.Get the filled input data
         input = UICtrl.getinput();
-        console.log(input);
-        // 2.Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-        // 3.Add the new item to the user interface
-        UICtrl.addListItem(newItem,input.type);
+        if (input.description !== "" && !(isNaN(input.value)) && input.value > 0) {
+            // 2.Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+            // 3.Add the new item to the user interface
+            UICtrl.addListItem(newItem, input.type);
+            //Clear fields
+            UICtrl.clearFields();
+        }
+        else {
+            alert("Incorrect data input!!!");
+        }
 
-        //Clear fields
-        UICtrl.clearFields();
-        // 4.Calculate the budget
-        // 5.Display budget to the UI
+
+        //Calculate and display budget
+        updateBudget();
+
     }
 
     return {
         init: function () {
             setupEventListeners();
         },
-        testing: function () {
+        showData: function () {
             console.log(data);
         }
 
