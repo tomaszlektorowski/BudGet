f61b0b42cae17a10fea6faea009623bd7d5b8313
Tomek <lektor1993@wp.pl>
2018-12-04 10:08:51 +0100

    implemented backbone of modular components

---
+++ app.js
@@ -0,0 +1,53 @@
+
+var budgetController = (function () {
+
+})();
+
+var UIController = (function () {
+
+    var DOMstrings={
+        inputType:'.add__type',
+        inputDescription:'.add__description',
+        inputValue:'.add__value',
+        inputBtn:'.add__btn'
+    };
+
+    return {
+        getinput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+
+        },
+        getDOMstrings:function(){
+            return DOMstrings;
+        }
+    };
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+    //GLOBAL APP CONTROLLER
+var DOM=UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function () {
+        // 1.Get the filled input data
+        var input=UICtrl.getinput();
+        console.log(input);
+        // 2.Add the item to the budget controller
+        // 3.Add the new item to the user interface
+        // 4.Calculate the budget
+        // 5.Display budget to the UI
+    }
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+})(budgetController, UIController);
+
+
+
